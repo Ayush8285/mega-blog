b90bc7c4ea602ae1b9cfd504b2b4c2a5760cea85
Ayush8285 <113717319+Ayush8285@users.noreply.github.com>
2025-07-30 21:37:01 +0530

    image fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import image from '../assets/images.png';
 
 function Logo() {
   return (
     <div className="flex items-center gap-2 sm:gap-3">
       <img
-        src="../../public/images.png"
+        src={image}
         alt="Logo"
         className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 object-contain"
       />
